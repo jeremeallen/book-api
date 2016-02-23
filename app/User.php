61de9260420e3f56d0f482bc676b8752b15ac95d
Jereme Allen <jereme.allen@wheniwork.com>
2016-02-22 19:11:17 -0600

    Finish off server code

--- app/User.php
+++ app/User.php
@@ -37,6 +37,11 @@ class User extends Model implements AuthenticatableContract,
      */
     protected $hidden = ['password', 'remember_token'];
 
+    public function books()
+    {
+        return $this->hasMany('App\Book');
+    }
+    
     /**
      * This mutator automatically hashes the password.
      *
